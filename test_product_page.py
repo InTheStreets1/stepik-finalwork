ab1290ca719e9180a213fdfc3dcdf5ec7670a264
InTheStreets1 <inthestreets@protonmail.com>
2019-07-29 02:33:37 +0300

    test update

--- test_product_page.py
+++ test_product_page.py
@@ -17,6 +17,7 @@ class TestUserAddToCartFromProductPage(object):
         page.open()
         page.can_add_product_to_cart()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_cart(self, browser):
         product_link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
         page = ProductPage(browser, product_link)
@@ -24,7 +25,7 @@ class TestUserAddToCartFromProductPage(object):
         page.should_be_button_add_product()
         page.add_product()
 
-
+@pytest.mark.need_review
 def test_guest_can_add_product_to_cart(browser):
     product_link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, product_link)
@@ -38,9 +39,18 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+	page = ProductPage(browser, link)
+	page.open()
+	page.go_to_cart_page()
+	page.should_not_be_product_in_cart()
+	page.see_alert_about_empty_cart()
